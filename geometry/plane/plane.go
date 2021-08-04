c8774a74cb2753ac0e5272daec18e0b4e7dbaa26
Minke Zhang <minke.zhang@gmail.com>
2021-08-03 21:25:33 -0700

    Add HP.in()

--- geometry/plane/plane.go
+++ geometry/plane/plane.go
@@ -8,14 +8,32 @@ import (
 type HP struct {
 	p vector.V
 	n vector.V
+
+	lCache vector.V
 }
 
 func New(p vector.V, n vector.V) *HP {
-	return &HP{n: n, p: p}
+	return &HP{
+		n:      n,
+		p:      p,
+		lCache: *vector.New(-n.Y(), n.X()),
+	}
 }
 
 func (p HP) N() vector.V { return p.n }
 func (p HP) P() vector.V { return p.p }
+
+// l returns the characteristic line along the plane is bisected. Points to the
+// "left" of the line are not permissible.
+func (p HP) l() vector.V { return p.lCache }
+
+func (p HP) in(v vector.V) bool {
+	// Generate a vector with tail on l and pointing towards the input.
+	w := vector.Sub(v, p.P())
+
+	// Check relative orientation between w and l.
+	return vector.Determinant(w, p.l()) >= 0
+}
 
 func Within(a HP, b HP, tolerance float64) bool {
 	return vector.Within(a.N(), b.N(), tolerance) && vector.Within(a.P(), b.P(), tolerance)
---
+++ geometry/plane/plane_test.go
@@ -0,0 +1,88 @@
+package plane
+
+import (
+	"testing"
+
+	"github.com/downflux/orca/geometry/vector"
+)
+
+const tolerance = 1e-10
+
+func TestIn(t *testing.T) {
+	type check struct {
+		v    vector.V
+		want bool
+	}
+	testConfigs := []struct {
+		name  string
+		hp    HP
+		l     vector.V
+		tests []check
+	}{
+		{
+			name: "Horizontal",
+			hp: *New(
+				*vector.New(0, 0),
+				*vector.New(1, 0),
+			),
+			l: *vector.New(0, 1),
+			tests: []check{
+				{v: *vector.New(0, 0), want: true},
+				{v: *vector.New(1, 0), want: true},
+				{v: *vector.New(-1, 0), want: false},
+			},
+		},
+		{
+			name: "Vertical",
+			hp: *New(
+				*vector.New(0, 0),
+				*vector.New(0, 1),
+			),
+			l: *vector.New(-1, 0),
+			tests: []check{
+				{v: *vector.New(0, 0), want: true},
+				{v: *vector.New(0, 1), want: true},
+				{v: *vector.New(0, -1), want: false},
+			},
+		},
+		{
+			name: "Sloped",
+			hp: *New(
+				*vector.New(0, 0),
+				*vector.New(1, 1),
+			),
+			l: *vector.New(-1, 1),
+			tests: []check{
+				{v: *vector.New(0, 0), want: true},
+				{v: *vector.New(1, 1), want: true},
+				{v: *vector.New(-1, -1), want: false},
+			},
+		},
+		{
+			name: "SlopedOffset",
+			hp: *New(
+				*vector.New(0, 1),
+				*vector.New(1, 1),
+			),
+			l: *vector.New(-1, 1),
+			tests: []check{
+				{v: *vector.New(0, 0), want: false},
+				{v: *vector.New(1, 1), want: true},
+				{v: *vector.New(2, 2), want: true},
+			},
+		},
+	}
+
+	for _, c := range testConfigs {
+		t.Run(c.name, func(t *testing.T) {
+			for _, test := range c.tests {
+				if got := c.hp.l(); !vector.Within(got, c.l, tolerance) {
+					t.Fatalf("l() = %v, want = %v", got, c.l)
+				}
+				if got := c.hp.in(test.v); got != test.want {
+					t.Errorf("in(%v) = %v, want = %v", test.v, got, test.want)
+				}
+			}
+		})
+	}
+}
