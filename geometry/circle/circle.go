a97763af08ab792903cdd91f1c7d5adb24391d85
Minke Zhang <mzhang@blogzhang.com>
2021-09-10 12:09:30 -0700

    Add Circle struct

---
+++ geometry/circle/circle.go
@@ -0,0 +1,17 @@
+package circle
+
+import (
+	"github.com/downflux/orca/geometry/vector"
+)
+
+type C struct {
+	r float64
+	p vector.V
+}
+
+func New(r float64, p vector.V) *C {
+	return &C{ r: r, p: p }
+}
+
+func (c C) R() float64 { return c.r }
+func (c C) P() vector.V { return c.p }
