5fef88d5bc8a531e4479ce7bc338f09edddfec72
Minke Zhang <mzhang@blogzhang.com>
2021-10-22 14:23:42 -0700

    Add initial K-D tree implementation

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/downflux/orca
 go 1.16
 
 require (
+	github.com/kyroy/kdtree v0.0.0-20200419114247-70830f883f1d
 	gonum.org/v1/gonum v0.9.3
 	google.golang.org/grpc v1.39.0
 )
--- kd/kd.go
+++ kd/kd.go
@@ -1,40 +1,73 @@
+// Package kd surfaces a K-D tree implementation with API tailored for DownFlux.
+//
+// Package wraps github.com/kyroy/kdtree.
 package kd
 
 import (
-	"github.com/kyroy/kdtree/kdtree"
-	"github.com/kyroy/kdtree/points/point2d"
-	"github.com/downflux/orca/vector/vector"
+	"github.com/downflux/orca/geometry/vector"
+	"github.com/kyroy/kdtree"
 )
 
+// P is an exported data point interface that data should implement.
 type P interface {
+	X() float64
+	Y() float64
 }
 
-type T interface {
-	Insert(p P)
-	Remove(p P)
-	Intersect(t T) []P
-	Within(v vector.V, r float64) []P
-	Near(p P, n int) []P
+// point represents a wrapped data struct to be passed into the K-D tree
+// implementation.
+type point struct {
+	data P
 }
 
-type p struct {
-	v vector.V
+func (p point) Dimensions() int { return 2 }
+func (p point) Dimension(i int) float64 {
+	return []float64{
+		p.data.X(),
+		p.data.Y(),
+	}[i]
 }
 
-func (p p) Dimensions() int { return 2 }
-func (p p) Dimension(i int) {
-	if i == 0 { return p.v.X() }
-	return p.v.Y()
-}
-
+// T is a wrapped K-D tree object for DownFlux.
 type T struct {
-	t kdtree.Tree
+	t *kdtree.KDTree
 }
 
-func New() *T { return &T }
-func Balance(t *T) { return t.Balance() }
+func New() *T { return &T{t: kdtree.New(nil)} }
 
-func (t *T) Insert(p p) { return t.t.Insert(p) }
-func (t *T) Remove(p p) { return t.t.Remove(p) }
-func (t *T) Intersect(r []vector.V) []p { ... }
+// Balance rebalances the K-D tree by reconstructing the internal
+// representation.
+func (t *T) Balance() { t.t.Balance() }
 
+// Insert adds a new data point into the K-D tree.
+func (t *T) Insert(p P) { t.t.Insert(point{data: p}) }
+
+// Remove deletes a data point from the K-D tree.
+func (t *T) Remove(p P) { t.t.Remove(point{data: p}) }
+
+// Within returns a list of points contained in a ball at the input coordinates
+// with the given radius.
+func (t *T) Within(v vector.V, r float64) []P {
+	var ps []P
+	for _, n := range t.t.RangeSearch([][2]float64{
+		[2]float64{v.X() - r, v.X() + r},
+		[2]float64{v.Y() - r, v.Y() + r},
+	}) {
+		p := n.(*point).data
+		if vector.SquaredMagnitude(
+			vector.Sub(v, *vector.New(p.X(), p.Y()))) <= r*r {
+			ps = append(ps, p)
+		}
+	}
+	return ps
+}
+
+// Neighbors returns the nearest N neighbors centered at the specified point.
+func (t *T) Neighbors(p P, n int) []P {
+	var ps []P
+	for _, n := range t.t.KNN(point{data: p}, n) {
+		ps = append(ps, n.(*point).data)
+	}
+
+	return ps
+}
