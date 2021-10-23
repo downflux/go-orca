741da68a90521d7aa5ddaeda6c54e728ed85ba94
Minke Zhang <minke.zhang@gmail.com>
2021-10-22 22:49:51 -0700

    Add K-T tree node documentation

--- kd/node/node.go
+++ kd/node/node.go
@@ -1,3 +1,4 @@
+// Package node implements a K-D tree node.
 package node
 
 import (
@@ -8,18 +9,28 @@ import (
 	"github.com/downflux/orca/kd/point"
 )
 
+// N represents a K-D tree node. Child nodes are sorted along the same axis,
+// with coordinates in the left node preceeding right node coordinates (along
+// the given axis).
 type N struct {
 	l *N
 	r *N
 
+	// depth represents the current level of the tree; the root node has a
+	// depth of 0.
 	depth int
 
-	v    vector.V
+	// v represents the coordinate of all data stored in this node.
+	v vector.V
+
+	// data is a list of data points stored in this node. All data here are
+	// located at the same spacial coordinate.
 	data []point.P
 }
 
 func (n *N) axis() axis.Type { return axis.A(n.depth) }
 
+// Data returns all data contained in the current and child nodes.
 func (n *N) Data() []point.P {
 	if n == nil {
 		return nil
@@ -34,6 +45,8 @@ func (n *N) Data() []point.P {
 	return data
 }
 
+// Insert inserts a data point into the node. The point may be stored inside a
+// child node.
 func (n *N) Insert(p point.P) {
 	x := axis.X(p.V(), n.axis())
 	nx := axis.X(n.v, n.axis())
@@ -59,6 +72,8 @@ func (n *N) Insert(p point.P) {
 	}
 }
 
+// Remove deletes a data point from the node or child nodes. A returned value of
+// false indicates the given point was not found.
 func (n *N) Remove(p point.P) bool {
 	x := axis.X(p.V(), n.axis())
 	nx := axis.X(n.v, n.axis())
@@ -78,6 +93,7 @@ func (n *N) Remove(p point.P) bool {
 	return false
 }
 
+// New returns a new K-D tree node instance.
 func New(data []point.P, depth int) *N {
 	if len(data) == 0 {
 		return nil
---
+++ kd/node/node_test.go
@@ -0,0 +1,5 @@
+package node
+
+import (
+	"testing"
+)
