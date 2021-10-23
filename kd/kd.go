f0d214743079d82d79fb519ee95d793512b88fa3
Minke Zhang <minke.zhang@gmail.com>
2021-10-23 01:36:17 -0700

    Fix K-D tree Node API compatibility

--- kd/kd.go
+++ kd/kd.go
@@ -10,13 +10,13 @@ type T struct {
 	root *node.N
 }
 
-func New(ps []point.P) *T {
+func New(ps []point.P, tolerance float64) *T {
 	return &T{
-		root: node.New(ps, 0),
+		root: node.New(ps, 0, tolerance),
 	}
 }
 
 func (t *T) Insert(p point.P)      { t.root.Insert(p) }
 func (t *T) Remove(p point.P) bool { return t.root.Remove(p) }
 
-func Balance(t *T) *T { return New(t.root.Data()) }
+func Balance(t *T, tolerance float64) *T { return New(t.root.Data(), tolerance) }
