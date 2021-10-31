a549e6dbf7bcbdd93f07df74e7e556befd792ea7
Minke Zhang <minke.zhang@gmail.com>
2021-10-31 16:10:41 -0700

    Add full rangesearch implementation

--- kd/rangesearch/rangesearch.go
+++ kd/rangesearch/rangesearch.go
@@ -7,10 +7,12 @@ import (
 
 	"github.com/downflux/orca/geometry/rectangle"
 	"github.com/downflux/orca/geometry/vector"
-	"github.com/downflux/orca/kd/node"
 	"github.com/downflux/orca/kd/axis"
+	"github.com/downflux/orca/kd/node"
 )
 
+// Search traverses the K-D tree node and returns all child nodes which are
+// contained in the bounding rectangle.
 func Search(n *node.N, r rectangle.R) []*node.N {
 	return search(n, r, *rectangle.New(
 		*vector.New(math.Inf(-1), math.Inf(-1)),
@@ -18,33 +20,48 @@ func Search(n *node.N, r rectangle.R) []*node.N {
 	))
 }
 
+// search recursively travels through the tree node to look for nodes within the
+// input rectangle. The bounding rectangle is a recursion artifact which keeps
+// track of the bounding box of the current node.
 func search(n *node.N, r rectangle.R, bound rectangle.R) []*node.N {
+	if _, ok := r.Intersect(bound); !ok {
+		return nil
+	}
+
 	var ns []*node.N
 
 	if r.In(n.V()) {
 		ns = append(ns, n)
 	}
 
-	min := map[axis.Type]rectangle.R{
+	// Calculate the new bounding boxes of the child nodes.
+	lb := map[axis.Type]rectangle.R{
 		axis.Axis_X: *rectangle.New(
-			*vector.New(math.Inf(-1), n.V().X()),
-			*vector.New(math.Inf(-1), math.Inf(0)),
+			bound.Min(),
+			*vector.New(n.V().X(), bound.Max().Y()),
 		),
 		axis.Axis_Y: *rectangle.New(
-			*vector.New(math.Inf(-1), n.V().X()),
-			*vector.New(math.Inf(-1), math.Inf(0)),
+			bound.Min(),
+			*vector.New(bound.Max().X(), n.V().Y()),
 		),
-	}
+	}[n.Axis()]
+	rb := map[axis.Type]rectangle.R{
+		axis.Axis_X: *rectangle.New(
+			*vector.New(n.V().X(), bound.Min().Y()),
+			bound.Max(),
+		),
+		axis.Axis_Y: *rectangle.New(
+			*vector.New(bound.Min().X(), n.V().Y()),
+			bound.Max(),
+		),
+	}[n.Axis()]
 
-	lb := *rectangle.New(
-		bound.Min(),
-		bound.Max(),
-	)
-	rb := *rectangle.New(
-		bound.Min(),
-		bound.Max(),
-	)
-	if _, ok := 
+	if c := n.L(); c != nil {
+		ns = append(ns, search(c, r, lb)...)
+	}
+	if c := n.R(); c != nil {
+		ns = append(ns, search(c, r, rb)...)
+	}
 
 	return ns
 }
