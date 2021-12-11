5a2857ea784ab882e353ad718fa5faac4e074613
Minke Zhang <minke.zhang@gmail.com>
2021-12-10 23:36:44 -0800

    Update animations

--- internal/orca/orca.go
+++ internal/orca/orca.go
@@ -14,6 +14,10 @@ func R(a agent.A, tau float64) float64 {
 	return math.Min(
 		math.Max(
 			100*tau*a.S(),
+			// We want to ensure at least one agent in a collision
+			// system will see its neighbors -- this ensures the
+			// largest agent will always move to avoid smaller
+			// agents.
 			3*a.R(),
 		),
 		6*a.R(),
