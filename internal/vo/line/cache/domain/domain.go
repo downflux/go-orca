7c8553230204af8197ca6047ea20e3b528a5cfda
Minke Zhang <mzhang@blogzhang.com>
2022-04-09 14:10:38 -0700

    Factor out left and right cone calculations for line VO

--- internal/vo/line/cache/domain/domain.go
+++ internal/vo/line/cache/domain/domain.go
@@ -1,14 +1,25 @@
 package domain
 
-type D int
+type D string
 
 const (
 	// Collision p-domains indicate a physical overlap between the obstacle
 	// and the agent.
 
-	CollisionLeft D = iota
-	CollisionRight
-	CollisionLine
+	CollisionLeft  D = "COLLISION_LEFT"
+	CollisionRight   = "COLLISION_RIGT"
+	CollisionLine    = "COLLISION_LINE"
 
-	Normal
+	// Circle v-domains indicate the velocity vector w lies closest to the
+	// turncation circles.
+
+	CircleLeft  = "CIRCLE_LEFT"
+	CircleRight = "CIRCLE_RIGHT"
+
+	// Linear v-domains indicate the velocity vector w lies closest to the
+	// straight lines of the line VO.
+
+	Left  = "LEFT"
+	Right = "RIGHT"
+	Line  = "LINE"
 )
