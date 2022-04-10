d82474bd3c76ff69cf57790f4259ec875b09ebd2
Minke Zhang <mzhang@blogzhang.com>
2022-04-10 15:15:54 -0700

    Add initial commit for calculating left / right VO domains

--- internal/geometry/2d/vector/vector_test.go
+++ internal/geometry/2d/vector/vector_test.go
@@ -7,58 +7,58 @@ import (
 )
 
 func TestIsNormalOrientation(t *testing.T) {
-	testConfigs := []struct{
+	testConfigs := []struct {
 		name string
-		v vector.V
-		u vector.V
+		v    vector.V
+		u    vector.V
 		want bool
 	}{
 		{
 			name: "Parallel",
-			v: *vector.New(1, 0),
-			u: *vector.New(1, 0),
+			v:    *vector.New(1, 0),
+			u:    *vector.New(1, 0),
 			want: true,
 		},
 		{
 			name: "Parallel/NonUnit",
-			v: *vector.New(2, 0),
-			u: *vector.New(5, 0),
+			v:    *vector.New(2, 0),
+			u:    *vector.New(5, 0),
 			want: true,
 		},
 		{
 			name: "AntiParallel",
-			v: *vector.New(1, 0),
-			u: *vector.New(-1, 0),
+			v:    *vector.New(1, 0),
+			u:    *vector.New(-1, 0),
 			want: false,
 		},
 		{
 			name: "AntiParallel/NonUnit",
-			v: *vector.New(2, 0),
-			u: *vector.New(-5, 0),
+			v:    *vector.New(2, 0),
+			u:    *vector.New(-5, 0),
 			want: false,
 		},
 		{
 			name: "Normal",
-			v: *vector.New(1, 0),
-			u: *vector.New(0, 1),
+			v:    *vector.New(1, 0),
+			u:    *vector.New(0, 1),
 			want: true,
 		},
 		{
 			name: "Normal/LargeTheta",
-			v: *vector.New(1, 0),
-			u: *vector.New(-1, 1),
+			v:    *vector.New(1, 0),
+			u:    *vector.New(-1, 1),
 			want: true,
 		},
 		{
 			name: "NonNormal",
-			v: *vector.New(1, 0),
-			u: *vector.New(0, -1),
+			v:    *vector.New(1, 0),
+			u:    *vector.New(0, -1),
 			want: false,
 		},
 		{
 			name: "NonNormal/LargeTheta",
-			v: *vector.New(1, 0),
-			u: *vector.New(-1, -1),
+			v:    *vector.New(1, 0),
+			u:    *vector.New(-1, -1),
 			want: false,
 		},
 	}
--- internal/vo/line/cache/cache_test.go
+++ internal/vo/line/cache/cache_test.go
@@ -1,8 +1,6 @@
 package cache
 
 import (
-	"fmt"
-	"math"
 	"testing"
 
 	"github.com/downflux/go-geometry/2d/line"
@@ -12,116 +10,6 @@ import (
 
 	mock "github.com/downflux/go-orca/internal/agent/testdata/mock"
 )
-
-func TestL(t *testing.T) {
-	type config struct {
-		name string
-		c    C
-		l    vector.V
-		r    vector.V
-	}
-
-	testConfigs := append(
-		[]config{},
-		func() []config {
-			// s is a horizontal line segment spanning (-1, 1) to
-			// (1, 1).
-			s := *segment.New(
-				*line.New(
-					*vector.New(-1, 1),
-					*vector.New(1, 0),
-				),
-				0,
-				2,
-			)
-			tau := 1.0
-			v := *vector.New(0, 0)
-			r := 1.0
-
-			return []config{
-				{
-					name: "Normal",
-					c: *New(
-						s,
-						v,
-						*mock.New(
-							mock.O{
-								P: *vector.New(0, 0),
-								R: r,
-							},
-						),
-						tau,
-					),
-					// L points away from the agent.
-					l: *vector.New(-1, 0),
-					// R points toward the agent.
-					r: *vector.New(-1, 0),
-				},
-				{
-					name: "Normal/Mirror",
-					c: *New(
-						s,
-						v,
-						*mock.New(
-							mock.O{
-								P: *vector.New(0, 2),
-								R: r,
-							},
-						),
-						tau,
-					),
-					l: *vector.New(1, 0),
-					r: *vector.New(1, 0),
-				},
-				{
-					name: "Oblique/Left",
-					c: *New(
-						s,
-						v,
-						*mock.New(
-							mock.O{
-								P: *vector.New(-3, 1),
-								R: r,
-							},
-						),
-						tau,
-					),
-					l: *vector.New(1.5, math.Sqrt(3)/2),
-					r: *vector.New(-1.5, math.Sqrt(3)/2),
-				},
-				{
-					name: "Oblique/Right",
-					c: *New(
-						s,
-						v,
-						*mock.New(
-							mock.O{
-								P: *vector.New(3, 1),
-								R: r,
-							},
-						),
-						tau,
-					),
-					l: *vector.New(-1.5, -math.Sqrt(3)/2),
-					r: *vector.New(1.5, -math.Sqrt(3)/2),
-				},
-			}
-		}()...,
-	)
-
-	for _, c := range testConfigs {
-		t.Run(fmt.Sprintf("%v/L", c.name), func(t *testing.T) {
-			if got := c.c.l(); !vector.Within(c.l, got) {
-				t.Errorf("l() = %v, want = %v", got, c.l)
-			}
-		})
-		t.Run(fmt.Sprintf("%v/R", c.name), func(t *testing.T) {
-			if got := c.c.r(); !vector.Within(c.r, got) {
-				t.Errorf("r() = %v, want = %v", got, c.r)
-			}
-		})
-	}
-}
 
 func TestDomain(t *testing.T) {
 	type config struct {
@@ -215,24 +103,6 @@ func TestDomain(t *testing.T) {
 		t.Run(c.name, func(t *testing.T) {
 			if got := c.c.domain(); got != c.want {
 				t.Errorf("domain() = %v, want = %v", got, c.want)
-			}
-		})
-	}
-}
-
-func TestW(t *testing.T) {
-	type config struct {
-		name string
-		c    C
-		want vector.V
-	}
-
-	testConfigs := []config{}
-
-	for _, c := range testConfigs {
-		t.Run(c.name, func(t *testing.T) {
-			if got := c.c.w(); !vector.Within(got, c.want) {
-				t.Errorf("w() = %v, want = %v", got, c.want)
 			}
 		})
 	}
