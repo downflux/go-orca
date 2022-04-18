5efcbbf9a4d858f7f8735d1c3fb5681efe0fb8cd
Minke Zhang <minke.zhang@gmail.com>
2022-04-18 00:24:15 -0700

    Update VO.ORCA interface documentation

--- internal/vo/vo.go
+++ internal/vo/vo.go
@@ -10,5 +10,12 @@ import (
 )
 
 type VO interface {
+	// ORCA is a hyperplane in 2D ambient space of the form HP(p, n), i.e.
+	// the boundary of the plane passes through p, and faces "into" the
+	// planar normal n. We consider points (i.e. velocities) in the plane to
+	// be feasible values for the input agent.
+	//
+	// More concisely, if HP.In(v), then v is a permissible agent velocity
+	// fo the current simulation snapshot.
 	ORCA(a agent.A, tau float64) hyperplane.HP
 }
