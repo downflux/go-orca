d0638cd5de8f1ca6c409e2ba9c9205e98fd7b037
Minke Zhang <minke.zhang@gmail.com>
2022-05-22 16:29:35 -0700

    Fix wall mock to handle collision cases

--- internal/vo/wall/cache/mock/mock.go
+++ internal/vo/wall/cache/mock/mock.go
@@ -6,6 +6,7 @@ import (
 	"github.com/downflux/go-geometry/2d/hyperplane"
 	"github.com/downflux/go-geometry/2d/segment"
 	"github.com/downflux/go-geometry/2d/line"
+	"github.com/downflux/go-orca/internal/vo/wall/cache/domain"
 	"github.com/downflux/go-geometry/2d/vector"
 )
 
@@ -26,6 +27,34 @@ func New(obstacle segment.S) *VO {
 	}
 }
 
+func (vo VO) domain(agent agent.A) domain.D {
+	rp1 := vector.Sub(
+		vo.obstacle.L().L(vo.obstacle.TMin()),
+		agent.P(),
+	)
+	rp2 := vector.Sub(
+		vo.obstacle.L().L(vo.obstacle.TMax()),
+		agent.P(),
+	)
+
+	d1 := vector.Magnitude(rp1)
+	d2 := vector.Magnitude(rp2)
+	dl := vo.obstacle.L().Distance(agent.P())
+
+	t := vo.obstacle.L().T(agent.P())
+	if t <= vo.obstacle.TMin() && d1 <= agent.R() {
+		return domain.CollisionLeft
+	}
+	if t >= vo.obstacle.TMax() && d2 <= agent.R() {
+		return domain.CollisionRight
+	}
+	if vo.obstacle.TMin() <= t && t <= vo.obstacle.TMax() && dl <= agent.R() {
+		return domain.CollisionLine
+	}
+
+	panic("unhandled domain case")
+}
+
 func (vo VO) ORCA(agent agent.A) hyperplane.HP {
 	rp1 := vector.Sub(
 		vo.obstacle.L().L(vo.obstacle.TMin()),
@@ -35,32 +64,30 @@ func (vo VO) ORCA(agent agent.A) hyperplane.HP {
 		vo.obstacle.L().L(vo.obstacle.TMax()),
 		agent.P(),
 	)
-	d1 := vector.Magnitude(rp1)
-	d2 := vector.Magnitude(rp2)
+	t := vo.obstacle.L().T(agent.P())
 
-	t := vo.obstacle.T(rp1)
-	d := vector.Magnitude(vector.Sub(agent.P(), vo.obstacle.L().L(t)))
-
-	// Agent collides with the left vertex
-	if t < 0 && d1 <= agent.R() {
+	switch d := vo.domain(agent); d {
+	case domain.CollisionLeft:
 		return *hyperplane.New(
-			*vector.New(0, 0),
-			vector.Unit(*vector.New(-rp1.Y(), rp1.X())),
+			*vector.New(0, 0),  // VOpt
+			vector.Unit(*vector.New(-rp1.X(), -rp1.Y())),
 		)
-	// Agent collides with the right vertex
-	} else if t > 1 && d2 <= agent.R() {
+	case domain.CollisionRight:
 		return *hyperplane.New(
 			*vector.New(0, 0),
-			vector.Unit(*vector.New(-rp2.Y(), rp2.X())),
+			vector.Unit(*vector.New(-rp2.X(), -rp2.Y())),
 		)
-	// Agent collides with the obstacle
-	} else if t >= 0 && t <= 1 && d < agent.R() {
+	case domain.CollisionLine:
 		return *hyperplane.New(
 			*vector.New(0, 0),
-			vector.Unit(vector.Sub(agent.P(), vo.obstacle.L().L(t))),
+			vector.Unit(
+				vector.Sub(
+					agent.P(),
+					vo.obstacle.L().L(t),
+				),
+			),
 		)
 	}
 
-	l1 :=
 	panic("unhandled ORCA case")
 }
