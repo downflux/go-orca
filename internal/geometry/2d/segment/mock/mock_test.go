2efe1a986b8ac83fa113477bd6a1cb2bf2e710db
Minke Zhang <mzhang@blogzhang.com>
2022-05-29 15:48:02 -0700

    Add constructor tests for mock wall segment

--- internal/geometry/2d/segment/mock/mock_test.go
+++ internal/geometry/2d/segment/mock/mock_test.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/downflux/go-geometry/2d/line"
 	"github.com/downflux/go-geometry/2d/segment"
 	"github.com/downflux/go-geometry/2d/vector"
 )
@@ -13,11 +14,101 @@ func TestCollision(t *testing.T) {
 		name string
 
 		obstacle segment.S
-		p vector.V
-		radius float64
+		p        vector.V
+		radius   float64
 
 		success bool
 	}{
+		{
+			name: "Collision/Left",
+			obstacle: *segment.New(
+				*line.New(
+					*vector.New(-1, 1),
+					*vector.New(1, 0),
+				),
+				0,
+				2,
+			),
+			p:      *vector.New(-1.5, 1),
+			radius: 1,
+
+			success: false,
+		},
+		{
+			name: "Collision/Right",
+			obstacle: *segment.New(
+				*line.New(
+					*vector.New(-1, 1),
+					*vector.New(1, 0),
+				),
+				0,
+				2,
+			),
+			p:      *vector.New(1.5, 1),
+			radius: 1,
+
+			success: false,
+		},
+		{
+			name: "Collision/Line",
+			obstacle: *segment.New(
+				*line.New(
+					*vector.New(-1, 1),
+					*vector.New(1, 0),
+				),
+				0,
+				2,
+			),
+			p:      *vector.New(0, 1),
+			radius: 1,
+
+			success: false,
+		},
+		{
+			name: "Success/Left",
+			obstacle: *segment.New(
+				*line.New(
+					*vector.New(-1, 1),
+					*vector.New(1, 0),
+				),
+				0,
+				2,
+			),
+			p:      *vector.New(-100, 1),
+			radius: 1,
+
+			success: true,
+		},
+		{
+			name: "Success/Right",
+			obstacle: *segment.New(
+				*line.New(
+					*vector.New(-1, 1),
+					*vector.New(1, 0),
+				),
+				0,
+				2,
+			),
+			p:      *vector.New(100, 1),
+			radius: 1,
+
+			success: true,
+		},
+		{
+			name: "Success/Line",
+			obstacle: *segment.New(
+				*line.New(
+					*vector.New(-1, 1),
+					*vector.New(1, 0),
+				),
+				0,
+				2,
+			),
+			p:      *vector.New(0, 100),
+			radius: 1,
+
+			success: true,
+		},
 	}
 
 	for _, c := range configs {
@@ -35,11 +126,10 @@ func TestTangents(t *testing.T) {
 	configs := []struct {
 		name string
 
-		s    S
-		l    vector.V
-		r    vector.V
-	}{
-	}
+		s S
+		l vector.V
+		r vector.V
+	}{}
 
 	for _, c := range configs {
 		t.Run(fmt.Sprintf("%v/L", c.name), func(t *testing.T) {
