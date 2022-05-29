c1764212a82668fbd6121325a3b4a4aa03ee6405
Minke Zhang <mzhang@blogzhang.com>
2022-05-29 15:41:15 -0700

    Add mock test framework

--- internal/geometry/2d/segment/mock/mock_test.go
+++ internal/geometry/2d/segment/mock/mock_test.go
@@ -4,16 +4,42 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/downflux/go-geometry/2d/segment"
 	"github.com/downflux/go-geometry/2d/vector"
 )
+
+func TestCollision(t *testing.T) {
+	configs := []struct {
+		name string
+
+		obstacle segment.S
+		p vector.V
+		radius float64
+
+		success bool
+	}{
+	}
+
+	for _, c := range configs {
+		t.Run(c.name, func(t *testing.T) {
+			if _, err := New(c.obstacle, c.p, c.radius); c.success && err != nil {
+				t.Errorf("New() = _, %v, want = _, nil", err)
+			} else if !c.success && err == nil {
+				t.Errorf("New() = _, nil, want a non-nil error")
+			}
+		})
+	}
+}
 
 func TestTangents(t *testing.T) {
 	configs := []struct {
 		name string
+
 		s    S
 		l    vector.V
 		r    vector.V
-	}{}
+	}{
+	}
 
 	for _, c := range configs {
 		t.Run(fmt.Sprintf("%v/L", c.name), func(t *testing.T) {
