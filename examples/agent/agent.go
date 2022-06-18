c24a64295c652795cde1dcdcfdead073de51bad4
Minke Zhang <mzhang@blogzhang.com>
2022-06-18 15:50:12 -0700

    Intermediate commit

--- examples/agent/agent.go
+++ examples/agent/agent.go
@@ -1,8 +1,6 @@
 package agent
 
 import (
-	"math"
-
 	"github.com/downflux/go-geometry/2d/vector"
 	"github.com/downflux/go-geometry/epsilon"
 	"github.com/downflux/go-orca/agent"
@@ -37,20 +35,36 @@ type A struct {
 	r float64
 }
 
-func (a *A) P() vector.V { return a.p }
-func (a *A) T() vector.V {
+// d returns the direction the agent should travel in.
+func (a *A) d() vector.V {
 	v := vector.Sub(a.G(), a.P())
-	if epsilon.Within(vector.SquaredMagnitude(v), 0) {
+	// Ensure when we're close enough, we stop.
+	if epsilon.Absolute(a.R()).Within(vector.SquaredMagnitude(v), 0) {
 		return *vector.New(0, 0)
 	}
-
-	// Reduce end-state jittering by forcing the agent to slow down.
-	s := math.Min(a.S(), a.S()*vector.SquaredMagnitude(v))
-	return vector.Scale(s, vector.Unit(v))
+	return vector.Unit(v)
 }
+
+// S returns the maximum speed of the agent. We are setting the agent speed to
+// be invariant -- this means agents can still move to avoid other agents even
+// after arriving at the destination.
+func (a *A) S() float64 { return a.s }
+
+// T returns the target velocity vector of the agent. In the demos provided, we
+// are setting the target to stop when "close enough" to the actual goal to
+// remove end-state jitter while preserving the flexibility agent to still
+// navigate to avoid collisions.
+func (a *A) T() vector.V {
+	d := vector.Sub(a.G(), a.P())
+	if epsilon.Within(vector.SquaredMagnitude(d), 0) {
+		d = *vector.New(0, 0)
+	}
+	return vector.Scale(a.S(), vector.Unit(d))
+}
+
+func (a *A) P() vector.V     { return a.p }
 func (a *A) V() vector.V     { return a.v }
 func (a *A) G() vector.V     { return a.g }
 func (a *A) R() float64      { return a.r }
-func (a *A) S() float64      { return a.s }
 func (a *A) SetV(v vector.V) { a.v = v }
 func (a *A) SetP(v vector.V) { a.p = v }
